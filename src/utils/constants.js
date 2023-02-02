6d5ea294efd7118b152b714a2aa25b617b8a0842
ManishAwhade <manish.awhade>
2023-02-02 14:16:18 +0530

    flight search dummy data

---
+++ src/utils/constants.js
@@ -0,0 +1,59 @@
+const FLIGHTSEARCHLIST = [
+  {
+    id: 1,
+    fromBoardingDate: "06-02-2023",
+    fromBoardingTime: "7:15 PM",
+    fromBoardingLocation: "Dubai DXB",
+    fromDurationType: "Non Stop",
+    fromDurationTime: "3 hrs 10 mins",
+    fromArrivalLocation: "Mumbai BOM",
+    fromArrivalDate: "06-02-2023",
+    fromArrivalTime: "11:55 PM",
+    fromBoardingDate: "22-02-2023",
+    fromBoardingTime: "4:25 PM",
+    fromBoardingLocation: "Mumbai BOM",
+    fromDurationType: "Non Stop",
+    fromDurationTime: "3 hrs 10 mins",
+    fromArrivalLocation: "Dubai DXB",
+    fromArrivalDate: "22-02-2023",
+    fromArrivalTime: "6:15 PM",
+  },
+  {
+    id: 2,
+    fromBoardingDate: "06-02-2023",
+    fromBoardingTime: "7:15 PM",
+    fromBoardingLocation: "Kenya",
+    fromDurationType: "Non Stop",
+    fromDurationTime: "3 hrs 10 mins",
+    fromArrivalLocation: "Mumbai BOM",
+    fromArrivalDate: "06-02-2023",
+    fromArrivalTime: "11:55 PM",
+    fromBoardingDate: "22-02-2023",
+    fromBoardingTime: "4:25 PM",
+    fromBoardingLocation: "Mumbai BOM",
+    fromDurationType: "Non Stop",
+    fromDurationTime: "3 hrs 10 mins",
+    fromArrivalLocation: "Kenya",
+    fromArrivalDate: "22-02-2023",
+    fromArrivalTime: "6:15 PM",
+  },
+];
+
+const AIRLINELIST = [
+  {
+    id: 1,
+    name: "Indigo",
+  },
+  {
+    id: 2,
+    name: "Vistara",
+  },
+  {
+    id: 3,
+    name: "Air India",
+  },
+  {
+    id: 4,
+    name: "Kuwait Airways",
+  },
+];
